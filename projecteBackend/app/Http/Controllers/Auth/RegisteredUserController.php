bf1180309702c028c371e1c1081355b0045d42b7
Toni Mira Esteve <tonimiraesteve@gmail.com>
2025-02-03 18:31:40 +0100

    Add username and surnames fields to registration; enhance login form layout

--- projecteBackend/app/Http/Controllers/Auth/RegisteredUserController.php
+++ projecteBackend/app/Http/Controllers/Auth/RegisteredUserController.php
@@ -33,12 +33,16 @@ class RegisteredUserController extends Controller
             'name' => ['required', 'string', 'max:255'],
             'email' => ['required', 'string', 'lowercase', 'email', 'max:255', 'unique:'.User::class],
             'password' => ['required', 'confirmed', Rules\Password::defaults()],
+            'username' => ['required', 'string', 'max:255', 'unique:users'],
         ]);
 
         $user = User::create([
             'name' => $request->name,
             'email' => $request->email,
             'password' => Hash::make($request->password),
+            'surnames' => $request->surnames,
+            'hireDate' => now(),
+            'username' => $request->name . $request->surnames
         ]);
 
         event(new Registered($user));
