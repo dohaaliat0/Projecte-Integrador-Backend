bebbe52ce76435950ff18eecf3897481181e9707
Toni Mira Esteve <tonimiraesteve@gmail.com>
2025-02-04 19:54:50 +0100

    Enhance dashboard image styling and update header gradient colors for improved UI

--- projecteBackend/resources/views/dashboard.blade.php
+++ projecteBackend/resources/views/dashboard.blade.php
@@ -13,8 +13,8 @@
         <div class="border-l-2 border-gray-300 mx-4"></div>
 
         <!-- Imagen que cambia -->
-        <div class="w-3/4 p-4">
-            <img :src="imageSrc" alt="Image" class="w-full h-auto object-cover transition-all duration-500 ease-in-out">
+        <div class="w-3/4 ">
+            <img :src="imageSrc" alt="Image" class="imagen w-full h-auto object-cover transition-all duration-500 ease-in-out">
         </div>
     </div>
 </x-app-layout>
