866dcdde4bb9ae88261e2edd76931d2ad43f5b46
Toni Mira Esteve <tonimiraesteve@gmail.com>
2025-02-04 20:34:58 +0100

    Update image dimensions and enhance layout styling for dashboard

--- projecteBackend/resources/views/dashboard.blade.php
+++ projecteBackend/resources/views/dashboard.blade.php
@@ -1,11 +1,11 @@
 <x-app-layout>
     <div x-data="{ imageSrc: '/images/default.jpg' }" class="flex">
         <!-- Sidebar de Links -->
-        <div class="w-1/4 p-4 space-y-4">
+        <div class="w-2/4 p-4 space-y-4">
             <a href="#" @mouseover="imageSrc = '/images/userImage.jpg'" class="link">Link 1</a>
-            <a href="#" @mouseover="imageSrc = '/images/image2.jpg'" class="link">Link 2</a>
+            <a href="#" @mouseover="imageSrc = '/images/DownUsers.jpg'" class="link">Link 2</a>
             <hr class="border-t-2 border-gray-300">
-            <a href="#" @mouseover="imageSrc = '/images/image3.jpg'" class="link">Link 3</a>
+            <a href="#" @mouseover="imageSrc = '/images/UpUsers.jpg'" class="link">Link 3</a>
             <a href="#" @mouseover="imageSrc = '/images/image4.jpg'" class="link">Link 4</a>
         </div>
 
@@ -13,8 +13,8 @@
         <div class="border-l-2 border-gray-300 mx-4"></div>
 
         <!-- Imagen que cambia -->
-        <div class="w-3/4 ">
-            <img :src="imageSrc" alt="Image" class="imagen w-full h-auto object-cover transition-all duration-500 ease-in-out">
+        <div class="w-2/4 w-70">
+            <img :src="imageSrc" alt="Image" class="justify-content-center h-auto object-cover transition-all duration-500 ease-in-out">
         </div>
     </div>
 </x-app-layout>
