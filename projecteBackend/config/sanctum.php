1c11975fd1b556f757830ad72b4c03f2f7662343
Marc Mollá Lillo <marmollil@alu.edu.gva.es>
2025-02-11 21:14:58 +0100

    Update CORS configuration to include new allowed origin for production environment

--- projecteBackend/config/sanctum.php
+++ projecteBackend/config/sanctum.php
@@ -17,7 +17,7 @@ return [
 
     'stateful' => explode(',', env('SANCTUM_STATEFUL_DOMAINS', sprintf(
         '%s%s',
-        'localhost,localhost:5173,localhost:5174,your-vue-app-domain.com,localhost:3000,127.0.0.1,127.0.0.1:8000,::1',
+        'localhost,localhost:5173,localhost:5174,http://conectasalud.batoi.es,localhost:3000,127.0.0.1,127.0.0.1:8000,::1',
         Sanctum::currentApplicationUrlWithPort()
     ))),
 
