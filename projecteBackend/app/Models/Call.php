4febc7bca5a1a21771c15119962b7bbf05918e19
Toni Mira Esteve <tonimiraesteve@gmail.com>
2025-02-18 23:35:30 +0100

    Add debugging statements in Call model to inspect created and updated events

--- projecteBackend/app/Models/Call.php
+++ projecteBackend/app/Models/Call.php
@@ -33,10 +33,12 @@ class Call extends Model
     protected static function booted()
     {
         static::created(function ($call) {
+            dd($call);
             event(new LlamadaActualizada($call));
         });
     
         static::updated(function ($call) {
+            dd($call);
             event(new LlamadaActualizada($call));
         });
     }
