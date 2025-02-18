116860db5c85363cdaaa3a85e16491cf7297d1dd
Marc Mollá Lillo <marmollil@alu.edu.gva.es>
2025-02-18 16:44:22 +0100

    Comment out the label method in Language enum for future reference

--- projecteBackend/app/Enums/Language.php
+++ projecteBackend/app/Enums/Language.php
@@ -35,17 +35,17 @@ enum Language: string
     case SWAHILI = 'Swahili';
     case AMHARIC = 'Amharic';
 
-    public function label(): string
-    {
-        return match ($this) {
-            self::SPANISH => 'Español',
-            self::CATALAN => 'Català',
-            self::GALICIAN => 'Galego',
-            self::BASQUE => 'Euskara',
-            self::ARANESE => 'Aranès',
-            default => $this->value, 
-        };
-    }
+    // public function label(): string
+    // {
+    //     return match ($this) {
+    //         self::SPANISH => 'Español',
+    //         self::CATALAN => 'Català',
+    //         self::GALICIAN => 'Galego',
+    //         self::BASQUE => 'Euskara',
+    //         self::ARANESE => 'Aranès',
+    //         default => $this->value, 
+    //     };
+    // }
 
     public static function values(): array
     {
