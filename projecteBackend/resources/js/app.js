00be117764dda7ea425170953d1d73ca76713dfe
Marc Mollá Lillo <marmollil@alu.edu.gva.es>
2025-02-20 00:58:10 +0100

    Update Pusher configuration in app.js to use correct environment variables

--- projecteBackend/resources/js/app.js
+++ projecteBackend/resources/js/app.js
@@ -10,13 +10,16 @@ import Echo from 'laravel-echo';
 import Pusher from 'pusher-js';
 
 window.Pusher = Pusher;
+console.log(import.meta.env.PUSHER_APP_KEY)
+console.log(import.meta.env.PUSHER_APP_CLUSTER)
 
 window.Echo = new Echo({
     broadcaster: 'pusher',
-    key: import.meta.env.VITE_PUSHER_APP_KEY,
-    cluster: import.meta.env.VITE_PUSHER_APP_CLUSTER,
+    key: import.meta.env.PUSHER_APP_KEY,
+    cluster: import.meta.env.PUSHER_APP_CLUSTER,
     forceTLS: true,
 });
+
 
 window.Echo.channel('ConectaSalud')
     .listen('.LlamadaActualizada', (e) => {
