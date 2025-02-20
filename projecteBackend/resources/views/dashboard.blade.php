b29686009084ed93ebf9ae5dee64e41220fa281d
Toni Mira Esteve <tonimiraesteve@gmail.com>
2025-02-20 09:53:38 +0100

    Refactor CSS layout and improve responsiveness for dashboard components

--- projecteBackend/resources/views/dashboard.blade.php
+++ projecteBackend/resources/views/dashboard.blade.php
@@ -1,17 +1,21 @@
 <x-app-layout>
-        <div x-data="{ imageSrc: '/images/userImage.jpg' }" class="relative background-container">
-            
-            <div :style="{ backgroundImage: 'url(' + imageSrc + ')' }" class="absolute inset-0 background-image"></div>
+    @vite('resources/css/index.css')
+    <div class="container">
+        <div x-data="{ imageSrc: '/images/users.png' }" class="layout-container">
+            <!-- Sidebar con enlaces -->
+            <div class="sidebar">
+                <a href="http://conectasalud.batoi.es" @mouseover="imageSrc = '/images/users.png'" class="link">Usuarios</a>
+                <a href="altabaja" @mouseover="imageSrc = '/images/asignarOperadores.png'" class="link">Dar de baja y alta</a>
+                <a href="webzones" @mouseover="imageSrc = '/images/Zonas.png'" class="link">Zonas</a>
+                <a href="assignusers" @mouseover="imageSrc = '/images/AltaBaja.png'" class="link">Asignar usuarios</a>
+                <a href="llamadas" @mouseover="imageSrc = '/images/llamadas.png'" class="link">Llamadas</a>
+            </div>
 
-            <div class="dark-overlay"></div>
-
-            <div class="w-2/4 p-4 space-y-4 sidebar">
-                <a href="http://conectasalud.batoi.es" @mouseover="imageSrc = '/images/userImage.jpg'" class="link">Usuarios</a>
-                <a href="altabaja" @mouseover="imageSrc = '/images/DownUsers.jpg'" class="link">Dar de baja y alta</a>
-                <a href="webzones" @mouseover="imageSrc = '/images/UpUsers.jpg'" class="link">Zonas</a>
-                <a href="assignusers" @mouseover="imageSrc = '/images/ModifiUser.jpg'" class="link">Asignar usuarios</a>
-                <a href="llamadas" @mouseover="imageSrc = '/images/ModifiUser.jpg'" class="link">Llamadas</a>
+            <!-- Contenedor de la imagen a la derecha -->
+            <div class="image-container">
+                <div :style="{ backgroundImage: 'url(' + imageSrc + ')' }" class="background-image"></div>
+                <div class="dark-overlay"></div>
             </div>
         </div>
+    </div>
 </x-app-layout>
-
